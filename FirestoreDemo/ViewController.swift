ed52ff4e15e6e2eff07c0f32968826ac306f92fa
puspakirana <puspakiranastv@gmail.com>
2025-03-22 21:30:48 +0700

    set up

---
+++ FirestoreDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirestoreDemo
+//
+//  Created by Puspakirana Stavira on 22/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
